800b4f403e690abd1343dd3f43640528b3cd665d
Daniel Howlett <dan@dancras.co.uk>
2017-11-28 20:49:19 +0100

    Add phpcs with wordpress code style

--- README.md
+++ README.md
@@ -16,3 +16,9 @@ If using docker-machine you'll probably need to forward ports:
 `VBoxManage controlvm default natpf1 "tcp8080,tcp,127.0.0.1,8080,,8080"`
 
 `VBoxManage controlvm default natpf1 "tcp22222,tcp,127.0.0.1,22222,,22222"`
+
+## Setting up PHP dependencies
+
+- `brew tap homebrew/homebrew-php`
+- `brew install homebrew/php/composer`
+- `composer install`
--- src/wordpress-version-control.php
+++ src/wordpress-version-control.php
@@ -1,16 +1,22 @@
 <?php
-/*
-Plugin Name: Wordpress Version Control
-Plugin URI: https://github.com/dancras/wordpress-version-control
-Description: TBD
-Version: 0.0.1
-Author: Daniel Howlett
-Author URI: http://dancras.co.uk/
-License: MIT
-*/
+/**
+ * Description: TBD
+ *
+ * Plugin Name: WordPress Version Control
+ * Plugin URI: https://github.com/dancras/wordpress-version-control
+ * Version: 0.0.1
+ * Author: Daniel Howlett
+ * Author URI: http://dancras.co.uk/
+ * License: MIT
+ *
+ * @package wordpress-version-control
+ */
 
-add_action('admin_notices', 'wordpress_version_control_hello_world');
+add_action( 'admin_notices', 'wordpress_version_control_hello_world' );
 
-function wordpress_version_control_hello_world () {
-    echo '<div class="notice notice-info"><p>Hello world</p></div>';
+/**
+ * Output a hello world notice
+ */
+function wordpress_version_control_hello_world() {
+	echo '<div class="notice notice-info"><p>Hello world</p></div>';
 }
