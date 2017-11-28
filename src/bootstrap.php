86b11bf47de33efb3cf0ca09646da3b88276848f
Daniel Howlett <dan@dancras.co.uk>
2017-11-29 00:12:05 +0100

    Add a side menu item and front controller

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -24,3 +24,25 @@ add_action( 'admin_notices', 'wordpress_version_control\hello_world' );
 function hello_world() {
 	echo '<div class="notice notice-info"><p>Hello world</p></div>';
 }
+
+add_action( 'admin_menu', 'wordpress_version_control\add_to_settings_menu' );
+
+/**
+ * Add a link to the plugin settings.
+ */
+function add_to_settings_menu() {
+	add_submenu_page(
+		'options-general.php',
+		__( 'Version Control', 'version-control-title' ),
+		__( 'Version Control', 'version-control-title' ),
+		'manage_options',
+		'version-control',
+		'wordpress_version_control\front_controller'
+	);
+}
+
+/**
+ * Handle all actions available to the plugin.
+ */
+function front_controller() {
+}
