12193d3446348875995f20c28eba4e4c75f0b867
Daniel Howlett <dan@dancras.co.uk>
2017-11-29 01:22:47 +0100

    Add version selector for current user

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -16,6 +16,18 @@
 
 namespace wordpress_version_control;
 
+/**
+ * Temporary fixed versions list
+ */
+function get_versions() {
+	return [
+		'master',
+		'feature',
+	];
+}
+
+add_action( 'admin_bar_menu', 'wordpress_version_control\add_version_selector' );
+
 add_action( 'admin_notices', 'wordpress_version_control\hello_world' );
 
 /**
@@ -26,6 +38,8 @@ function hello_world() {
 }
 
 add_action( 'admin_menu', 'wordpress_version_control\add_to_settings_menu' );
+
+add_action( 'wp_loaded', 'wordpress_version_control\action_controller' );
 
 /**
  * Add a link to the plugin settings.
@@ -44,5 +58,76 @@ function add_to_settings_menu() {
 /**
  * Handle all actions available to the plugin.
  */
+function action_controller() {
+	$version = filter_input( INPUT_GET, 'version-control', FILTER_SANITIZE_STRING );
+
+	if ( ! empty( $version ) ) {
+		update_user_option(
+			get_current_user_id(),
+			'version-control-selected-version',
+			$version
+		);
+	}
+}
+
+/**
+ * Handle all actions available to the plugin.
+ */
 function front_controller() {
+}
+
+/**
+ * Add version selector to the secondary toolbar.
+ *
+ * @param WP_Admin_Bar $wp_admin_bar - Use to modify admin toolbar.
+ */
+function add_version_selector( $wp_admin_bar ) {
+
+	$versions         = get_versions();
+	$selected_version = get_selected_version();
+
+	$node = array(
+		'id'     => 'version-control-version-selector',
+		'parent' => 'top-secondary',
+		'title'  => "Version: $selected_version",
+	);
+
+	$wp_admin_bar->add_node( $node );
+
+	foreach ( $versions as $version ) {
+
+		if ( $version === $selected_version ) {
+			continue;
+		}
+
+		$node = array(
+			'id'     => "version-control-version-selector-$version",
+			'parent' => 'version-control-version-selector',
+			'title'  => "Select: $version",
+			'href'   => get_select_version_url( $version ),
+		);
+
+		$wp_admin_bar->add_node( $node );
+
+	}
+}
+
+/**
+ * Get the configured option for selected version.
+ *
+ * @return String
+ */
+function get_selected_version() {
+	return get_user_option( 'version-control-selected-version' ) ?: 'master';
+}
+
+/**
+ * Get a url for selecting a version.
+ *
+ * @param String $version - The version to be selected.
+ *
+ * @return String
+ */
+function get_select_version_url( $version ) {
+	return admin_url( 'index.php' ) . "?version-control=$version";
 }
